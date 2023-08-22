01be633d28a73d3f8227365a4de977d1fd4ecaa8
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-08-22 14:35:18 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/vite-typescript-template",
+  build: {
+    outDir: "build", // Здесь указываем папку "build"
+  },
+});
