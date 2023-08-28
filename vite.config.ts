2548c6fa5859f8f47b7cd61fea3855857481c484
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-08-28 20:47:46 +0300

    change configs for import

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@components": "/src/components",
+    },
+  },
   plugins: [react()],
-  base: "/vite-typescript-template",
+  base: "/react-web-messenger",
   build: {
     outDir: "build", // Здесь указываем папку "build"
   },
