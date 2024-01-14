3ca521e267f4a71db3b85206371241ea38632c2d
BohdanKushnerov <bohdan.kushnerov@gmail.com>
2024-01-14 09:30:22 +0000

    Deploying to gh-pages from @ BohdanKushnerov/react-web-messenger@7dd89dee641160f8ba6431b3e3354448ba7485e3 🚀

---
+++ assets/useStartTransition-ee180a68.js
@@ -0,0 +1,1 @@
+import{r}from"./index-f89e3274.js";const a=()=>{const[s,t]=r.useState(!1);return r.useEffect(()=>(t(!0),()=>{t(!1)}),[]),s};export{a as u};
--- assets/useStartTransition-f553dfc0.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-fe14b15b.js";const a=()=>{const[s,t]=r.useState(!1);return r.useEffect(()=>(t(!0),()=>{t(!1)}),[]),s};export{a as u};
