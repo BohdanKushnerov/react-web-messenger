5348ce68181a4e04f3b825df1a49d6a2c4ff9892
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2024-02-26 17:23:49 +0200

    fixed selection of default language when loading the app

--- src/hooks/useDefaultLanguage.tsx
+++ src/hooks/useDefaultLanguage.tsx
@@ -1,6 +1,11 @@
 import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const useDefaultLanguage = () => {
+  const { i18n } = useTranslation('translation', {
+    keyPrefix: 'NavBar',
+  });
+
   useEffect(() => {
     if (localStorage.language) {
       return;
@@ -10,14 +15,18 @@ const useDefaultLanguage = () => {
 
     if (userLanguage === 'en-US') {
       localStorage.setItem('language', 'en');
+      i18n.changeLanguage('en');
     } else if (userLanguage === 'uk-UA') {
       localStorage.setItem('language', 'ua');
+      i18n.changeLanguage('ua');
     } else if (userLanguage === 'ru-RU') {
       localStorage.setItem('language', 'ru');
+      i18n.changeLanguage('ru');
     } else {
       localStorage.setItem('language', 'en');
+      i18n.changeLanguage('en');
     }
-  }, []);
+  }, [i18n]);
 };
 
 export default useDefaultLanguage;
