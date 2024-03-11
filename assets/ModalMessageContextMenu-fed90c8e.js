793342d39edbe2df8330318df882e5f41b8afb7d
BohdanKushnerov <bohdan.kushnerov@gmail.com>
2024-03-11 15:24:15 +0000

    Deploying to gh-pages from @ BohdanKushnerov/react-web-messenger@cf922b0cb6efd3dee42f0ec90240af13060eb162 🚀

--- assets/ModalMessageContextMenu-fed90c8e.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-795f580d.js";import{u as n}from"./useCloseModal-24cf0242.js";const p=({closeModal:t,modalPosition:e,children:s})=>(n(t),o.jsx("div",{style:{position:"absolute",top:e.top+"px",left:e.left+"px"},className:"z-50 w-screen h-screen bg-transparent pointer-events-none",children:s}));export{p as default};
