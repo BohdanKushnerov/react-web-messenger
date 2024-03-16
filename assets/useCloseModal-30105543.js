9f20a26482d608402954a656288dd0ad14a6a4ce
BohdanKushnerov <bohdan.kushnerov@gmail.com>
2024-03-16 11:36:51 +0000

    Deploying to gh-pages from @ BohdanKushnerov/react-web-messenger@8a1a5aaaa1a517a81cbc50642af53fe8a48acce3 🚀

--- assets/useCloseModal-30105543.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-a91ef496.js";const s=e=>{t.useEffect(()=>{const o=n=>{n.code==="Escape"&&e()};return window.addEventListener("keydown",o),()=>{window.removeEventListener("keydown",o)}},[e])};export{s as u};
---
+++ assets/useStartTransition-10875e17.js
@@ -0,0 +1,1 @@
+import{r}from"./index-4c86432f.js";const a=()=>{const[s,t]=r.useState(!1);return r.useEffect(()=>(t(!0),()=>{t(!1)}),[]),s};export{a as u};
